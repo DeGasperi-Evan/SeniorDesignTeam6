3cb6cc112a4a52fd6de18db45ea2015a79e2b3ee
NoicTheBrave <nicpizza2000@gmail.com>
2022-11-17 01:45:09 -0600

    Uploading Library that allows easy logging of timestamps. Intended to help log data and assist analysis.

---
+++ timePassed.h
@@ -0,0 +1,15 @@
+/*
+Author: Nicholas Chorette
+Date & Time: 11/17/2022 (@ 1:39am)
+Purpose: The purpose of this file is to link the "timePassed.c" file. to other C programs. 
+*/
+
+#include <stdio.h>
+#include <time.h>
+
+
+clock_t startT; // not sure if needed, but its here
+clock_t endT; // not sure if needed, but its here
+
+double timePassed(); 
+void timeInit(); 
