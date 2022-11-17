e65d5c7622bf2212aade68858e30915ee030786e
NoicTheBrave <nicpizza2000@gmail.com>
2022-11-17 12:48:06 -0600

    timePassed fix: data type conflict (maybe), added time var into dataCollection, Reverse made

---
+++ reverse.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+// #include "comms.h"// Disabled for testing due to the fact that i do not have comms.h and comms.c NOT on the laptop when testing compiling
+#include <time.h>
+
+
